dc1a6cb078c242218a7e0c90f44a04f23f5ec6dc
Luca Ostinelli <ostinelliluca2@gmail.com>
2023-11-03 08:20:51 +0100

    update

--- codice/server.c
+++ codice/server.c
@@ -257,9 +257,7 @@ int main(int argc, char* argv[]){
 					switch(tipo) {
 						case -1: // Si sta presentando
 							riceviLunghezza(i, &lmsg);
-							ret = ricevi(i, lmsg, &bufferOut);
-							printf(bufferOut);
-							fflush(stdout);
+							ricevi(i, lmsg, &bufferOut);
 							ret = inserisci(i, bufferOut[0]);
 							switch(ret) {
 								case -1:
