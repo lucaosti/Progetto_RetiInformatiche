fed94a28e0549ecceb66e373896ca1eaac76b557
Luca Ostinelli <ostinelliluca2@gmail.com>
2023-11-04 11:31:14 +0100

    update

--- codice/server.c
+++ codice/server.c
@@ -172,7 +172,7 @@ int main(int argc, char* argv[]){
 
 							// Invio ad ogni dispositivo connesso il messaggio "STOP"
 							strcpy(bufferOut,"STOP\0");
-							for(j = 1; j < fdmax; j++) {
+							for(j = 1; j <= fdmax; j++) {
 								if(j == listener) continue; // Salta il listener
 								if(!FD_ISSET(j, &master)) continue; // Nel caso lo abbia già chiuso, non scrivo
 								ret = invia(j, bufferOut);
